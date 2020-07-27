079e24eedcf327dc70658a7a59d4018b6da236a9
Alexandra Faller <alex@faller.us>
2020-07-27 16:14:28 +0100

    adding the async function using heroku for a proxy url to avoid the cors errors

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello");
+//get quote from api
+
+async function getQuote(){
+    const proxyUrl = "https://shrouded-sea-82269.herokuapp.com/"
+    const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        
+        console.log('Whoops, no quote', error);
+    }
+}
+
+//on load
+getQuote();
