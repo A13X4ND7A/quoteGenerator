6cf157a975a6821b1ba227c4d9f8f938517a87f1
Alexandra Faller <alex@faller.us>
2020-07-27 18:57:04 +0100

    added functionality to display the text from the api into the page. Added functionality for the buttons to get a new quote and to tweet the quote

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
-//get quote from api
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuotebtn = document.getElementById('new-quote');
 
+
+
+//get quote from api
 async function getQuote(){
     const proxyUrl = "https://shrouded-sea-82269.herokuapp.com/"
     const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        //if author is blank then add unknown as default
+        if (data.quoteAuthor === ''){
+            authorText.innerText = "unknown";
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        
+        //reduce fontsize for long quotes
+        if (data.quoteText.length > 120){
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+
+       
 
     } catch (error) {
+        getQuote();
         
-        console.log('Whoops, no quote', error);
+       
     }
 }
+
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//event listeners
+newQuotebtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //on load
 getQuote();
